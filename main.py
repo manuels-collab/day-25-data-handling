0e7c9e9f62d875ec2b4f5f0ff90d85627c126a9b
Manuel Prince <manuelsdesk0029@gmail.com>
2025-10-30 09:45:42 +0100

    ON DAY 27 i learnt a different thing

--- main.py
+++ main.py
@@ -1,59 +1,39 @@
-'''
-import csv
-from operator import index
+from tkinter import *
 
-with open("weather_data (2).csv") as data_file:
-    data = csv.reader(data_file)
-    temperature = []
-    for row in data:
-        if row[1] != "temp":
-            temperature.append(int((row[1])))
-    print(temperature)
+def calc_km():
+    km = first_entry.get()
+    answer = float(km) * 1.60934
+    result.config(text=f'{answer}')
 
+window = Tk()
+window.title("Mile to Km converter")
+window.minsize(width=500, height=300)
+window.config(padx=100, pady=100)
+#Label
 
+result = 0
 
+first_entry = Entry()
+first_entry.grid(row=0, column=1)
 
+first_label = Label(text="Miles")
+first_label.grid(row=0, column=2)
 
-temp_list = data["temp"].to_list()
+second_label = Label(text="is equal to")
+second_label.grid(row=1, column=0)
 
-max_value = data["temp"].max()
+result = Label(text=f"{result}")
+result.grid(row=1, column=1)
 
+third_label = Label(text="Km")
+third_label.grid(row=1,  column=2)
 
-#et Data in ro of our Data Frame
-max_temp = data[data.temp == data.temp.max()]
-print(max_temp)
-monday = data[data.day == 'Monday']
-fahrenheit_temps =( monday['temp'] * 9/5) + 32
-print(f"Fahrenheit temps: {fahrenheit_temps}")
-data = pandas.read_csv("weather_data (2).csv")
-
-
-# Creating a DATAFRAME from scratch
-data_dict = {
-    "students": ["Amy", "James", "Angela"],
-    "scores": [76, 56, 65]
-}
-data = pandas.DataFrame(data_dict)
-data.to_csv("new_data.csv")
-print(data)
+#Button
+my_button = Button(text="Calculate", command=calc_km)
+my_button.grid(row=2, column=1)
 
 
-'''
 
 
-import pandas
-data = pandas.read_csv('2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv')
-gray_squirrels = len(data[data["Primary Fur Color"] == "Gray"])
-red_squirrels = len(data[data["Primary Fur Color"] == "Cinnamon"])
-black_squirrels = len(data[data["Primary Fur Color"] == "Black"])
-print(gray_squirrels)
-#print(red_squirrels)
-#print(black_squirrels)
 
-data_dict = {
-    "Fur Color": ["Gray", "Cinnamon", "Black"],
-    "Count": [gray_squirrels, red_squirrels, black_squirrels],
-}
-
-#df = pandas.DataFrame(data_dict)
-#df.to_csv("squirrel_count.csv")
+window.mainloop()
---
+++ playground.py
@@ -0,0 +1,7 @@
+def add(*args):
+    number = 0
+    for num in args:
+        number += num
+    print(number)
+
+add(10, 20, 40, 50)
